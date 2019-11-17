4dbbb8269296ef48f373ca4246df0fd46168f83b
Wills Blake <richwblake@gmail.com>
2019-11-17 16:49:44 -0500

    Final commit, all tests pass

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+
+  attr_accessor :roster
+
+  def initialize( school )
+    @school = school
+    @roster = {}
+  end
+
+  def add_student( student_name, student_grade )
+    if roster[student_grade] == nil
+      roster[student_grade] = [student_name]
+    else
+      roster[student_grade].push( student_name)
+    end
+  end
+
+  def grade( grade )
+    roster[grade]
+  end
+
+  def sort
+    roster.each { |grade, name_list| name_list.sort! }
+  end
+
+
+end
